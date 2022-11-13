52781d26fee4bf5f0e88dd4ef702a5c5c4d2dbbb
MaxHiit <pro.maxcode@gmail.com>
2022-11-13 13:29:09 +0100

    feat(pages): integration of the pages of the app

---
+++ src/views/FavorisView.vue
@@ -0,0 +1,20 @@
+<template>
+  <BaseMovieListLayout
+    v-if="userFavMovies.length > 0"
+    :moviesList="userFavMovies"
+  />
+
+  <BaseMessage v-else message="You don't have any favorites yet" />
+</template>
+
+<script setup>
+import { storeToRefs } from "pinia";
+import { useMovieStore } from "@/stores/moviesStore";
+import BaseMovieListLayout from "@/components/BaseMovieListLayout.vue";
+import BaseMessage from "@/components/BaseMessage.vue";
+
+const movieStore = useMovieStore();
+const { userFavMovies } = storeToRefs(movieStore);
+</script>
+
+<style></style>
--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,11 +1,21 @@
-<script setup>
-import BaseFilterCategoryNav from "../components/list/BaseFilterCategoryNav.vue";
-import BaseCategoryMovieListLayout from "../components/list/BaseCategoryMovieListLayout.vue";
-</script>
-
 <template>
   <div class="flex flex-col md:flex-row">
     <BaseFilterCategoryNav />
-    <BaseCategoryMovieListLayout />
+    <BaseMovieListLayout
+      v-if="getFilterMovie.length > 0"
+      :moviesList="getFilterMovie"
+    />
+    <BaseMessage v-else message="no result" />
   </div>
 </template>
+
+<script setup>
+import { storeToRefs } from "pinia";
+import { useMovieStore } from "@/stores/moviesStore";
+import BaseFilterCategoryNav from "@/components/list/BaseFilterCategoryNav.vue";
+import BaseMovieListLayout from "@/components/BaseMovieListLayout.vue";
+import BaseMessage from "@/components/BaseMessage.vue";
+
+const movieStore = useMovieStore();
+const { getFilterMovie } = storeToRefs(movieStore);
+</script>
