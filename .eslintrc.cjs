d55d5e0cc95d8ba717b17438a208cfa20ea2eccc
MaxHiit <pro.maxcode@gmail.com>
2022-11-15 22:35:53 +0100

    chore(config): add new config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,19 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+  },
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier'
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-prettier",
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: "latest",
+  },
+};
